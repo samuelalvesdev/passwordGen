73ad3bbb294ce7d2a2abbd5656b0804ccf882f93
Samuel Alves <samuelalves@utic.pe.senac.br>
2023-09-27 00:40:59 -0300

    construção da lógica do genPass no JavaScript

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("teste");
+function generatePassword() {
+  const chars ="abcdefghjklmnpqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ123456789?!@*()[]"
+  
+  let password = ""
+  
+  for (let i = 0; i < 8; i++) {
+      const randomNumber = Math.floor(Math.random() * chars.length)
+      password += chars.substring(randomNumber, randomNumber + 1)
+    }
+    console.log(password)
+
+  }
+  
+  
+  generatePassword();
